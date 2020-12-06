52c442b89a9d8495381d698bec8b4a59c1f7bed0
Saif Ul Islam <saifulislam84210@gmail.com>
2020-12-06 17:45:22 +0500

    Twilio code bug fix # 6

--- src/routes/example.js
+++ src/routes/example.js
@@ -26,24 +26,24 @@ router.post('/send-sms', async (req, res, next) => {
   const { To, Body } = req.body;
   console.log(req.body);
   console.log(To, Body);
-  // try {
-  //   const { MessageSid } = await client.messages.create({
-  //     from: cfg.twilioPhoneNumber,
-  //     to: To,
-  //     body: Body,
-  //   });
+  try {
+    const { MessageSid } = await client.messages.create({
+      from: cfg.twilioPhoneNumber,
+      to: tostring(To),
+      body: toString(Body),
+    });
 
-  //   res.send({
-  //     status: 'success',
-  //     message: `SMS sent to ${req.body.To}. Message SID: ${MessageSid}`,
-  //   });
-  // } catch (err) {
-  //   console.error(err);
-  //   res.status(500).send({
-  //     status: 'error',
-  //     message: 'Failed to send SMS. Check server logs for more details.',
-  //   });
-  // }
+    res.send({
+      status: 'success',
+      message: `SMS sent to ${req.body.To}. Message SID: ${MessageSid}`,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({
+      status: 'error',
+      message: 'Failed to send SMS. Check server logs for more details.',
+    });
+  }
 });
 
 router.post('/message', async (req, res, next) => {
